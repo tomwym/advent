7001001e262092357bd2f51632dc3519af244a39
tomwym <tomwym@live.ca>
2021-12-17 18:51:24 -0500

    finished problem 06.1 and 06.2

--- 2021/include/05.hpp
+++ 2021/include/05.hpp
@@ -3,28 +3,28 @@
 namespace advent21 {
 
 template<typename T>
-struct Point {
+struct Point05 {
     T x;
     T y;
-    Point();
-    Point(T, T);
+    Point05();
+    Point05(T, T);
 };
 
 template<typename T>
-Point<T>::Point() : x(0), y(0) { } 
+Point05<T>::Point05() : x(0), y(0) { } 
 
 template<typename T>
-Point<T>::Point(T _x, T _y) : x(_x), y(_y) { }
+Point05<T>::Point05(T _x, T _y) : x(_x), y(_y) { }
 
 // https://stackoverflow.com/questions/3882467/defining-operator-for-a-struct
 template<typename T>
-bool operator<(const Point<T>& p1, const Point<T>& p2) {
+bool operator<(const Point05<T>& p1, const Point05<T>& p2) {
     return std::tie(p1.x, p1.y) < std::tie(p2.x, p2.y);
 }
 
 template<typename T>
-std::vector<Point<T>> FindLineCoverage(const int part, const Point<T>& p1, const Point<T>& p2) {
-    std::vector<Point<T>> obj {};
+std::vector<Point05<T>> FindLineCoverage(const int part, const Point05<T>& p1, const Point05<T>& p2) {
+    std::vector<Point05<T>> obj {};
 
     T Dx = p2.x-p1.x;
     T dx = Dx ? Dx/abs(Dx) : 0;
@@ -42,7 +42,7 @@ std::vector<Point<T>> FindLineCoverage(const int part, const Point<T>& p1, const
     for (int i=0; i<std::max(abs(Dx), abs(Dy)); i++) {
         x0 += dx;
         y0 += dy;
-        obj.push_back(Point(x0, y0));
+        obj.push_back(Point05(x0, y0));
     }
     return obj;
 }
@@ -66,16 +66,16 @@ Sol05<S,T>::Sol05(const std::string& nums)
 
 template<typename S, typename T>
 T Sol05<S,T>::Day05(const int part) {
-    std::map<Point<T>,T> mep {};
+    std::map<Point05<T>,T> mep {};
     for (const auto& vT : obj) {
         std::vector<T> vp1 = vTFromS<T>(vT[0], ',');
         std::vector<T> vp2 = vTFromS<T>(vT[2], ',');
 
-        Point<T> p1(vp1[0], vp1[1]);
-        Point<T> p2(vp2[0], vp2[1]);
-        std::vector<Point<T>> pointsAlongLine = FindLineCoverage(part, p1, p2);
+        Point05<T> p1(vp1[0], vp1[1]);
+        Point05<T> p2(vp2[0], vp2[1]);
+        std::vector<Point05<T>> Point05sAlongLine = FindLineCoverage(part, p1, p2);
 
-        for (const auto& pt : pointsAlongLine) {
+        for (const auto& pt : Point05sAlongLine) {
             auto mit = mep.find(pt);
             // key already exsits
             if (mit != mep.end()) {
---
+++ 2021/include/06.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace advent21 {
+
+template<typename T>
+class Sol06 : public Base {
+public:
+    Sol06(const std::string&);
+    void Solution1();
+    void Solution2();
+    T Day06(const T);
+private:
+    std::vector<std::vector<T>> obj;
+};
+
+template<typename T>
+Sol06<T>::Sol06(const std::string& nums)
+: Base(nums) { }
+
+template<typename T>
+T Sol06<T>::Day06(const T ITERATIONS) {
+    std::vector<T> container(9, 0);
+    // populate the container with values of input
+    for (const auto& i : obj[0]) {
+        container[i]++;
+    }
+    for (int i=0; i<ITERATIONS; i++) {
+        T buffer = container[0];
+        for (int i=0; i<=7; i++) {
+            container[i] = container[i+1];
+        }
+        container[6] += buffer;
+        container[8] = buffer;
+    }
+    // note: accumuate assumes input 0 as integer, so need to cast it to type T
+    T sum = std::accumulate(container.begin(), container.end(), static_cast<T>(0));
+    return sum;
+}
+
+
+template<typename T>
+void Sol06<T>::Solution1() {
+    obj = ReadVVT<T>(this->filename, ',');
+    T sum = Day06(80);
+    std::cout << sum << " solution1" << std::endl;
+}
+
+template<typename T>
+void Sol06<T>::Solution2() {
+    T sum = Day06(256);
+    std::cout << sum << " solution2" << std::endl;
+}
+
+}
