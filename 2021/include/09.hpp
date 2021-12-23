f122ca6db82b5b6145bbb0a93e79c230f74f4e9c
tomwym <tomwym@live.ca>
2021-12-23 04:11:10 -0500

    finished problems 09.1 and 09.2

--- 2021/include/09.hpp
+++ 2021/include/09.hpp
@@ -1,6 +1,65 @@
 #pragma once
 
 namespace advent21 {
+
+int convolve(const int i, const int j, const std::vector<std::string>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    std::vector<int> compared {};
+    int here = grid[i][j]-'0';
+    // north
+    if (!(i < 1)) {
+        compared.push_back(grid[i-1][j]-'0');
+    }
+    // south
+    if (!(i > m-2)) {
+        compared.push_back(grid[i+1][j]-'0');
+    }
+    // west
+    if (!(j < 1)) {
+        compared.push_back(grid[i][j-1]-'0');
+    }
+    // east
+    if (!(j > n-2)) {
+        compared.push_back(grid[i][j+1]-'0');
+    }
+    for (const auto& val : compared) {
+        if (val <= here) {
+            return 0;
+        }
+    }
+    std::cout << i << ' ' << j << ' ' << here << '\n';
+    return here+1;
+}
+
+bool look(size_t i, size_t j,
+          const std::vector<std::string>& out,
+          std::vector<std::vector<bool>>& checked,
+          int& size) {
+        
+    if (out[i][j] != '9' && !checked[i][j] ) {
+        size++;
+    } else {
+        return 0;
+    }
+    checked[i][j] = true;
+
+    if (!(i < 1)) {
+        look(i-1, j, out, checked, size);
+    }
+    if (!(i > out.size()-2)) {
+        look(i+1, j, out, checked, size);
+    }
+    if (!(j < 1)) {
+        look(i, j-1, out, checked, size);
+    }
+    if (!(j > out[0].size()-2)) {
+        look(i, j+1, out, checked, size);
+    }
+    return true;
+}
+
+
 template<typename T>
 class Sol09 : public Base {
 public:
@@ -21,120 +80,51 @@ T Sol09<T>::Day09() {
     return {};
 }
 
-
 template<typename T>
 void Sol09<T>::Solution1() {
     obj = ReadVVT<T>(this->filename, ',');
     std::vector<std::string> out = vTFromVVT<T>(obj);
     int sum = 0;
     // max indexes
-    int n = out[0].size()-1;
-    int m = out.size()-1;
+    int n = out[0].size();
+    int m = out.size();
     std::cout << "n: " << n << "\tm: " << m << '\n';
-    for (int i=1; i<m-1; i++) {
-        for (int j=1; j<n-1; j++) {
-            int H = (int)out[i][j];
-            int N = (int)out[i-1][j];
-            int S = (int)out[i+1][j];
-            int E = (int)out[i][j+1];
-            int W = (int)out[i][j-1];
-            if (H<N && H<S && H<E && H<W) {
-                std::cout << i << ',' << j << '\n';
-                sum += H+1;
-            }
-
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            sum += convolve(i, j, out);
         }
-    }
-    std::cout << "north " << '\n';
-    // north wall
-    for (int j=1; j<n-1; j++) {
-        int H = (int)out[0][j];
-        int S = (int)out[1][j];
-        int E = (int)out[0][j+1];
-        int W = (int)out[0][j-1];
-        if (H<S && H<E && H<W) {
-            std::cout << 0 << ',' << j << '\n';
-            sum += H+1;
-        }
-    }
-    std::cout << "south " << '\n';
-    // south wall
-    for (int j=1; j<n-1; j++) {
-        int H = (int)out[m][j];
-        int N = (int)out[m-1][j];
-        int E = (int)out[m][j+1];
-        int W = (int)out[m][j-1];
-        if (H<N && H<E && H<W) {
-            std::cout << m << ',' << j << '\n';
-            sum += H+1;
-        }
-    }
-    std::cout << "east " << '\n';
-    // east wall
-    for (int i=1; i<m-1; i++) {
-        int H = (int)out[i][n];
-        int N = (int)out[i-1][n];
-        int S = (int)out[i+1][n];
-        int W = (int)out[i][n-1];
-        if (H<N && H<S && H<W) {
-            std::cout << i << ',' << n << '\n';
-            sum += H+1;
-        }
-    }
-    std::cout << "west " << '\n';
-    // west wall
-    for (int i=1; i<m-1; i++) {
-        int H = (int)out[i][0];
-        int N = (int)out[i-1][0];
-        int S = (int)out[i+1][0];
-        int E = (int)out[i][1];
-        if (H<N && H<S && H<E) {
-            std::cout << i << ',' << 0 << '\n';
-            sum += H+1;
-        }
-    }
-    int H, N, S, E, W;
-    // northwest corner
-    H = (int)out[0][0];
-    S = (int)out[1][0];
-    E = (int)out[0][1];
-    if (H<S && H<E) {
-        std::cout << 0 << ',' << 0 << '\n';
-        sum += H+1;
-    }
-    // northeast corner
-    H = (int)out[0][n];
-    S = (int)out[1][n];
-    W = (int)out[0][n-1];
-    if (H<S && H<W) {
-        std::cout << 0 << ',' << n << '\n';
-        sum += H+1;
-    }
-    // southwest corner
-    H = (int)out[m][0];
-    N = (int)out[m-1][0];
-    E = (int)out[m][1];
-    if (H<N && H<E) {
-        std::cout << m << ',' << 0 << '\n';
-        sum += H+1;
-    }
-    // southeast corner
-    H = (int)out[m][n];
-    N = (int)out[m-1][n];
-    W = (int)out[m][n-1];
-    if (H<N && H<W) {
-        std::cout << m << ',' << n << '\n';
-        sum += H+1;
     }
 
     // 9483 too high
     // 9780 too high
+    // 1636 too high
     std::cout << sum << " solution1" << std::endl;
 }
 
+
 template<typename T>
 void Sol09<T>::Solution2() {
-    std::cout << " solution2" << std::endl;
+    std::vector<std::string> out = vTFromVVT<T>(obj);
+    std::vector<std::vector<bool>> checked(out.size(), std::vector<bool>(out[0].size(), false));
+    int m = out.size();
+    int n = out[0].size();
+    std::vector<int> container {};
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            if (!checked[i][i]) {
+                int sum = 0;
+                look(i, j, out, checked, sum);
+                container.push_back(sum);
+            }
+        }
+    }
+
+    std::sort(container.begin(), container.end());
+    int product = 1;
+    for (auto rit = container.rbegin(); rit != container.rbegin()+3; rit++) {
+        product *= *rit;
+    }
+    std::cout << product << " solution2" << std::endl;
 }
 
 }
