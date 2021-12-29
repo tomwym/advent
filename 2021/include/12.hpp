c2658c8f45799cf046b3ec19ff25b71e24232ea6
tomwym <tomwym@live.ca>
2021-12-29 18:24:04 -0500

    finished problem 12.1 and 12.2

--- 2021/include/12.hpp
+++ 2021/include/12.hpp
@@ -16,7 +16,7 @@ Node12::Node12() : nexts({nullptr}), name(""), size(false) { }
 
 Node12::Node12(const std::string& _name) : nexts({nullptr}), name(_name), size(false) {
     size = (bool)std::isupper(_name[0]);
-    std::cout << "inserting " << _name << " with size " << size << '\n';
+    // std::cout << "inserting " << _name << " with size " << size << '\n';
 }
 
 void Node12::insert(Node12* const _next) {
@@ -56,7 +56,7 @@ Node12* Node12_Container::find(const std::string& _name) {
     return nullptr;
 }
 
-// purely for finding the node with corresponding name
+// purely for finding the node in graph with corresponding name
 Node12* preTraversal(Node12* const root, const std::string& search, std::set<Node12*>& visited) {
     if (root == nullptr) {
         return nullptr;
@@ -83,57 +83,79 @@ Node12* preTraversal(Node12* const root, const std::string& search, std::set<Nod
     return nullptr;
 }
 
-void printPath(std::stack<std::string> path) {\
+void printPath(std::stack<std::string> path, const std::map<Node12*, int>& visited) {
     std::vector<std::string> temp {};
     while (!path.empty()) {
         temp.push_back(path.top());
         path.pop();
     }
+
     for (auto it=temp.rbegin(); it!=temp.rend(); it++) {
         std::cout << *it << ',';
     }
-    std::cout << '\n';
+    std::cout << "end" << '\n';
+}
+
+int nmaxval(const std::map<Node12*, int>& visited, const int limit) {
+    int n = 0;
+    for (const auto& p : visited) {
+        if (p.first != nullptr) {
+            if (p.second >= limit) {
+                n++;
+            }
+        }
+    }
+    return n;
 }
 
 int DEPTHFIRSTSEARCH_COUNT = 0;
-void depthFirstSearch(Node12* const root, std::set<Node12*>& visited, 
-                      std::stack<std::string>& path, Node12* const ROOT) {
+void depthFirstSearch(Node12* const root, std::map<Node12*, int>& visited, 
+                      std::stack<std::string>& path, Node12* const ROOT, const int key) {
     // conditions for backtracking // 
     // if root is null
     if (root == nullptr) {
         return;
     }
-    path.push(root->name);
+    // only visit the 'start' node once
+    if (root == ROOT && visited[root] > 0) {
+        return;
+    }
+    // if there is more than one node with more than one visit, return early
+    if (nmaxval(visited, 2)>1) {
+        return;
+    }
+    // if lower case, and has the key has been visited
+    // and if the number of times it is visited is already that specified by key
+    // then return early    
+    if (!root->size && visited.count(root) && visited[root]>=key) {
+        return;
+    // if lower case, and has the key has been visited
+    } else if (!root->size && visited.count(root)) {
+        visited[root]++;
+    // 
+    } else if (!root->size) {
+        visited[root] = 1;
+    }
 
     // if at the end of the maze
     if (root->name == "end") {
-        printPath(path);
+        // printPath(path, visited);
         // every time we backtrack we want to undo any insertions to visited
-        std::set<Node12*> t_visited {};
-        visited.erase(preTraversal(ROOT, path.top(), t_visited));
-        path.pop();
+        visited[root]--;
         DEPTHFIRSTSEARCH_COUNT++;
         return;
     }
 
-    // if lower case and has been visited
-    if (!root->size && visited.find(root) != visited.end()) {
-        path.pop();
-        return;
-    } else if (!root->size) {
-        visited.insert(root);
-    }
-
+    path.push(root->name);
     // otherwise keep searching
     for (const auto& n : root->nexts) {
         if (n != nullptr) {
-            depthFirstSearch(n, visited, path, ROOT);
+            depthFirstSearch(n, visited, path, ROOT, key);
         }
     }
     // why release before popping? wtf
     // same thing, backtrack while releasing top from visited set
-    std::set<Node12*> t_visited {};
-    visited.erase(preTraversal(ROOT, path.top(), t_visited));
+    visited[root]--;
     path.pop();
     return;
 }
@@ -147,25 +169,17 @@ public:
     T Day12();
 private:
     std::vector<std::vector<T>> obj;
+    // create the graph from the input and store in a container (RAII)
+    Node12_Container node_container;
 };
 
 template<typename T>
 Sol12<T>::Sol12(const std::string& nums)
-: Base(nums) { }
+: Base(nums), node_container({}) {
 
-template<typename T>
-T Sol12<T>::Day12() {
-    return { };
-}
-
-
-template<typename T>
-void Sol12<T>::Solution1() {
     obj = ReadVVT<T>(this->filename, '-');
     std::set<std::string> node_name_collection{};
 
-    // create the graph from the input and store in a container (RAII)
-    Node12_Container node_container;
     // first create nodes for all objects
     for (const auto& r : obj) {
         std::string s_parent({r[0]});
@@ -181,9 +195,8 @@ void Sol12<T>::Solution1() {
             node_name_collection.insert(s_child);
         }
     }
-
-    // link nodes together as a directed acyclic graph
-    Node12* const ROOT = node_container.find("start");
+    
+    // link nodes together as a graph
     for (const auto& r : obj) {
         std::string s_parent({r[0]});
         std::string s_child({r[1]});
@@ -193,32 +206,38 @@ void Sol12<T>::Solution1() {
         n_parent->insert(n_child);
         n_child->insert(n_parent);
     }
+}
 
-    // test preorder traversal algo here
-    for (const auto& r : obj) {
-        std::string s_parent({r[0]});
-        std::string s_child({r[1]});
-        Node12* n_parent;
-        Node12* n_child;
-        std::set<Node12*> p_visited {};
-        n_parent = preTraversal(ROOT, s_parent, p_visited);
-        std::set<Node12*> c_visited {};
-        n_child = preTraversal(ROOT, s_child, c_visited);
+template<typename T>
+T Sol12<T>::Day12() {
+    return { };
+}
 
-        // std::cout << n_parent << ' ' << n_parent->name << '\n';
-        // std::cout << n_child << ' ' << n_child->name << '\n' << '\n';
-    }
-    std::set<Node12*> visited {};
+
+template<typename T>
+void Sol12<T>::Solution1() {
+
+    Node12* const ROOT = node_container.find("start");
+
+    std::map<Node12*, int> visited {};
     std::stack<std::string> path {};
     DEPTHFIRSTSEARCH_COUNT = 0;
-    depthFirstSearch(ROOT, visited, path, ROOT);
+    depthFirstSearch(ROOT, visited, path, ROOT, 1);
 
     std::cout << DEPTHFIRSTSEARCH_COUNT << " solution1" << std::endl;
 }
 
 template<typename T>
 void Sol12<T>::Solution2() {
-    std::cout << " solution2" << std::endl;
-}
 
+    Node12* const ROOT = node_container.find("start");
+
+    std::map<Node12*, int> visited {};
+    std::stack<std::string> path {};
+    DEPTHFIRSTSEARCH_COUNT = 0;
+    depthFirstSearch(ROOT, visited, path, ROOT, 2);
+
+    std::cout << DEPTHFIRSTSEARCH_COUNT << " solution2" << std::endl;
+    
+}
 }
